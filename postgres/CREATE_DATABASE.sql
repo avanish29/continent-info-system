9de331c5c16b3193a9445eff28c0bfc548e90e1f
Avanish <avanish.k.pandey@gmail.com>
2020-11-30 01:28:02 +0530

    Change docker-compose.yml to pass active profile 'prod'

--- postgres/CREATE_DATABASE.sql
+++ postgres/CREATE_DATABASE.sql
@@ -1,6 +1,1 @@
 CREATE DATABASE nation;
-GRANT ALL PRIVILEGES ON DATABASE nation TO root;
---SELECT datname FROM pg_database WHERE datname='nation'';
---IF datname='nation'
---    THEN CREATE DATABASE nation PASSWORD 'root';
---END IF;
